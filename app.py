782cf4dddd430e2f393eace07a49afd75810cf99
vishnu venu <vishnuvenuvenk@gmail.com>
2020-02-24 17:03:03 +0530

    adding training

--- app.py
+++ app.py
@@ -3,21 +3,48 @@ from flask import Flask, request, jsonify
 import pickle
 from fastai.tabular import *
 import os
-from fastai.vision import open_image
+from fastai.vision import *
+from fastai.metrics import error_rate
+from fastai.widgets import *
 from io import BytesIO
+
 
 
 app = Flask(__name__)
 
 
+bs = 64
+
 cwd = os.getcwd()
-path = cwd + '/models'
+model_path = cwd + '/models'
+
+path=cwd + '/data'
+
+
+
+np.random.seed(42)
+data = ImageDataBunch.from_folder(path, train=".", valid_pct=0.2,
+        ds_tfms=get_transforms(), size=224, num_workers=4).normalize(imagenet_stats)
+
+learn = cnn_learner(data, models.resnet34, metrics=error_rate)
+
+learn.model
+
+learn.fit_one_cycle(4)
+
+learn.export()
 
 
 
 
 
-model = load_learner(path, 'export.pkl')
+
+
+
+
+
+
+model = load_learner(model_path, 'export.pkl')
 
 
 @app.route("/classify-url", methods=["POST"])
