07ccf6a1b9742b2e3de78e2a42b61a1246541ea6
vishnu venu <vishnuvenuvenk@gmail.com>
2020-02-25 14:17:59 +0530

    changed port number

--- app.py
+++ app.py
@@ -1,35 +1,20 @@
-
 from flask import Flask, request, jsonify
-
 import os
 from fastai.vision import *
-
 from io import BytesIO
 import sys
 from waitress import serve
 
 
-
-PORT = 5000
-
-
-
+PORT = 8080
 
 app = Flask(__name__)
-
-
-
 
 
 cwd = os.getcwd()
 model_path = cwd + '/models'
 
 learner = load_learner(model_path, 'model.pkl')
-
-
-
-
-
 
 
 def predict_image_from_bytes(bytes):
@@ -69,14 +54,5 @@ def upload():
 
 
 
-
-
-
-
-    
-
-
-
 if __name__ == '__main__':
-    # app.run(debug=True)
     serve(app, listen='*:{}'.format(str(PORT)))
